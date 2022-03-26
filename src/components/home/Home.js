8b118b19769f7eb932aa3b4095ba21c4045dab3c
Fran3x <franex@onet.eu>
2022-03-26 20:35:58 +0100

    Fetch classes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Home.css";
 import { Button, Divider } from "@mui/material";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
@@ -6,130 +6,164 @@ import { Stack } from "@mui/material";
 import { Box } from "@mui/material";
 import { AddBox } from "@material-ui/icons";
 
-const Home = () => {
-
-  var classes = [
-    {
-      id: 1,
-      name: "PZSP1"
-    },
-    {
-      id: 2,
-      name: "PZSP2"
-    },
-    {
-      id: 3,
-      name: "BSS"
-    },
-    {
-      id: 4,
-      name: "POP"
-    },
-    {
-      id: 5,
-      name: "WSI"
-    },
-    {
-      id: 6,
-      name: "MNUM"
-    }
-  ]
 
 
-  return (
-    <Stack
-      direction="row"
-      divider={<Divider orientation="vertical" flexItem style={{
-        marginRight:'50px'
-      }}/>}
-    >
-      {
-        classes.length > 0 
-        ?
-        <div className="classes-tiles" style={
-          {
-            'gridTemplateColumns': 'repeat(' + Math.min(classes.length, 4) +', 1fr)',
-            // 'display': 'flex',
-            'justify-content': 'center',
-            'align-items':'center',
-            'marginRight':'30px'
-          }
-        }>
-          {
-            classes.map((classObject) => 
-            
-            <Button 
-              component={Link} to={'/classes/' + classObject.name}
-              variant="contained" 
-              className="class-tile" 
-              key={classObject.id} 
-              style={
-                {
-                  'backgroundColor': "#91BBE7",
-                  'margin': '20px',
-                  'minHeight': '200px',
-                  'minWidth': '200px',
-                  'borderRadius': '24px',
-                  'fontSize': '25px'
+export default class Home extends Component  {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      classes: []
+    };
+
+    // url statyczny, do zmiany
+    fetch('/api/subjects/4')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.state.classes = data
+        this.setState({
+          classes: data
+        })
+      });
+  }
+
+  // example data
+  // [{
+  //   id: 1,
+  //   name: "PZSP1"
+  // },
+  // {
+  //   id: 2,
+  //   name: "PZSP2"
+  // },
+  // {
+  //   id: 3,
+  //   name: "BSS"
+  // },
+  // {
+  //   id: 4,
+  //   name: "POP"
+  // },
+  // {
+  //   id: 5,
+  //   name: "WSI"
+  // },
+  // {
+  //   id: 6,
+  //   name: "MNUM"
+  // }]
+
+  // componentDidMount(){
+  //   // url statyczny, do zmiany
+  //   fetch('/api/subjects/4')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data)
+  //       this.state.classes = data
+  //       this.setState({
+  //         classes: data
+  //       })
+  //     });
+  // }
+  
+
+
+  render(){
+
+    return (
+      <Stack
+        direction="row"
+        divider={<Divider orientation="vertical" flexItem style={{
+          marginRight:'50px'
+        }}/>}
+      >
+        {
+          this.state.classes.length > 0 
+          ?
+          <div className="classes-tiles" style={
+            {
+              'gridTemplateColumns': 'repeat(' + Math.min(this.state.classes.length, 4) +', 1fr)',
+              // 'display': 'flex',
+              'justify-content': 'center',
+              'align-items':'center',
+              'marginRight':'30px'
+            }
+          }>
+            {
+              this.state.classes.map((classObject) => 
+              
+              <Button 
+                component={Link} to={'/classes/' + classObject.name}
+                variant="contained" 
+                className="class-tile" 
+                key={classObject.id} 
+                style={
+                  {
+                    'backgroundColor': "#91BBE7",
+                    'margin': '20px',
+                    'minHeight': '200px',
+                    'minWidth': '200px',
+                    'borderRadius': '24px',
+                    'fontSize': '25px'
+                  }
                 }
-              }
-              >
-                  
-                <strong>{classObject.name}</strong>
-                  
-            </Button>
-            
-            )
-          }
-        </div>
-        :
-        <Box style={
-          {
-            'display':'flex',
-            'justify-content': 'center',
-            'align-items':'center',
-            'margin': '20px',
-            'height': '200px',
-            'width': '200px',
-            'borderRadius': '24px',
-            'fontSize': '25px',
-            'textAlign':'center'
-          }
-        }>
-          Start with creating your first subject!
-        </Box>
-      }
-
-
-
-      <Button  
-        variant="contained"  
-        component={Link} to={'/addNewClass'}
-        sx={{
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop: '20px',
-        marginBottom: '20px',
-        minWidth: '200px',
-        minHeight: '200px',
-        borderRadius: '24px',
-        
-      }}>
-        <AddBox style={{
-          width: '70px',
-          height: '70px'
+                >
+                    
+                  <strong>{classObject.name}</strong>
+                    
+              </Button>
+              
+              )
+            }
+          </div>
+          :
+          <Box style={
+            {
+              'display':'flex',
+              'justify-content': 'center',
+              'align-items':'center',
+              'margin': '20px',
+              'height': '200px',
+              'width': '200px',
+              'borderRadius': '24px',
+              'fontSize': '25px',
+              'textAlign':'center'
+            }
+          }>
+            Start with creating your first subject!
+          </Box>
+        }
+  
+  
+  
+        <Button  
+          variant="contained"  
+          component={Link} to={'/addNewClass'}
+          sx={{
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display:'flex',
+          justifyContent:'center',
+          alignItems:'center',
+          marginTop: '20px',
+          marginBottom: '20px',
+          minWidth: '200px',
+          minHeight: '200px',
+          borderRadius: '24px',
+          
         }}>
-
-        </AddBox>
-      </Button>
-    </Stack>
-  );
+          <AddBox style={{
+            width: '70px',
+            height: '70px'
+          }}>
+  
+          </AddBox>
+        </Button>
+      </Stack>
+    );
+  }
 };
-
-export default Home;
