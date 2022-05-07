4c5e4e902b18d17e38616d83f9b491110ecf97bf
Ignacy Tadeusz Kluszczyński <i.kluszczynski@stud.elka.pw.edu.pl>
2022-05-07 19:10:01 +0200

    adding person works

--- src/components/members/AddNewMember.js
+++ src/components/members/AddNewMember.js
@@ -9,12 +9,19 @@ import SendIcon from '@mui/icons-material/Send';
 import axios from "axios";
 import { useState, useEffect } from 'react';
 import { CompressOutlined } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import commons from '../../commons'
+
+
 
 
 const AddNewMember = ({props}) => {
+  const { class_id } = useParams()
   const [users, setUsers] = useState([])
   const [participants, setParticipants] = useState(props.location.state.participants)
-  const [role, setRole] = useState('WRITE')
+  const [person, setPerson] = useState('')
+  const [role, setRole] = useState('')
+  const [avUsers, setAvUsers] = useState([])
   const roles = [
       'ADMIN',
       'WRITE',
@@ -29,14 +36,48 @@ const AddNewMember = ({props}) => {
             setUsers(data)
             console.log('data', data)
             console.log('users', users)
-        });
+          });        
   }, []); 
+
+  useEffect(() => {
+
+    console.log("Changed users: ", users)
+    console.log("participants: ", participants)
+ 
+ }, [users])
 
   const availableUsers = (users, participants) => {
     var pId = new Set(participants.map(({ userId }) => userId))
     const available = users.filter((user) => !pId.has(user.id))
     return available
   }
+  useEffect(() => {
+    console.log("role: ", role)
+  }, [role])
+
+  const onAdd = (e) => {
+    e.preventDefault()
+    if(!person) {
+      alert('Please add a Person')
+      return
+    }
+    else if(!role) {
+      alert('Please add a role')
+      return
+    }
+    var body = { 
+      subjectId: class_id,
+      userId: person.id,
+      subjectRoles: []
+    }
+    axios.post(commons.baseURL + "/api/subjectuser/add", body)
+          .then(response => {
+            const data = response.data
+            console.log("ADD PERSON TO SUBJECT", data)
+          })
+          .catch(e => { return });
+    
+  };
 
   return (
     <Container>
@@ -45,43 +86,16 @@ const AddNewMember = ({props}) => {
         single
         id="tags-standard"
         options={availableUsers(users, participants)}
-        getOptionLabel={(option) => option.firstname}
+        getOptionLabel={(option) => `${option.firstname} ${option.lastname} : ${option.emial}`}
         renderInput={(params) => (
           <TextField
             {...params}
             variant="standard"
-            label="Name"
-            placeholder="Choose a Name"
+            label="Person"
+            placeholder="Choose a Person"
           />
         )}
-      />
-      <Autocomplete
-        single
-        id="tags-standard"
-        options={availableUsers(users, participants)}
-        getOptionLabel={(option) => option.lastname}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="Surname"
-            placeholder="Choose a Surname"
-          />
-        )}
-      />
-      <Autocomplete
-        single
-        id="tags-outlined"
-        options={availableUsers(users, participants)}
-        getOptionLabel={(option) => option.emial}
-        filterSelectedOptions
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="E-mail"
-            placeholder="Choose an e-mail"
-          />
-        )}
+        onChange={(event, value) => setPerson(value)}
       />
       <Autocomplete
         single
@@ -96,13 +110,16 @@ const AddNewMember = ({props}) => {
             placeholder="Choose a Position"
           />
         )}
+        onChange={(event, value) => setRole(value)}
       />
     </Stack>
     <Stack  direction="row" spacing={2} margin={5} justify-content="center" alignItems="center" sx={{ width: 500 }}>
-    <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => availableUsers(users, participants)}> 
-      Cancel
-    </Button>
-    <Button variant="contained" endIcon={<SendIcon />}>
+    <Link to={`/classes/${class_id}`} className="rounded-0">
+      <Button variant="outlined" startIcon={<DeleteIcon />} > 
+        Cancel
+      </Button>
+    </Link>
+    <Button variant="contained" endIcon={<SendIcon />} onClick={onAdd}>
       Submit
     </Button>
   </Stack>
